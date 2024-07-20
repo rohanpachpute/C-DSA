3ecb2b22f77bdfda645f1a32963321e7b95c1e2e
rohanpachpute <pachputerohan15@gmail.com>
2024-07-20 19:09:12 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*
+    TYPES OF DATA TYPES:-
+
+    int age = 25;
+    char name ='Rohan';
+    bool isadult =true;
+    float number = 132563726377;
+    double num2 = 232732183628736276;
+    cout<<"size of int is "<<sizeof(int)<<endl;
+    cout<<"size of char is "<<sizeof(char)<<endl;
+    cout<<"size of bool is "<<sizeof(bool)<<endl;
+    cout<<"size of float is "<<sizeof(float)<<endl;
+    cout<<"size of double is "<<sizeof(double)<<endl;
+*/
+
+/*
+ INPUT OUTPUT
+int age;
+cout<<"enter your age: ";
+cin>>age;
+ cout<<"Your age is"<<age;
+
+return 0;
+*/
+
+
+/*SUm OF TWO NUMBERS
+int a;
+int b;
+cout<<"enter value of a";
+cin>>a;
+cout<<"enter the value of b";
+cin>>b;
+int sum;
+cout<<"sum of a and b is "<<a+b;
+*/
+}
