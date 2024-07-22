42106dc3b165175599978233470a77f80ffcca6d
Rohan Pachpute <pachputerohan15@gmail.com>
2024-07-22 19:44:53 +0530

    Conditional Statements
    Today i'm Studing about Conditional Statements

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,59 @@
+// Today learing about Conditional Statements:-
+// There are mainly Three to Four Types:-
+// a)if
+// b)if-else
+// c)else-if(nested-if)
+// d)switch
+
+// Starting
+// a)if: in if only if conditional only Return there is no other condition.
+
+// b)if-else:- in this if the if condition is true then code in if block will print, else code in block of else will be print.
+// #include <iostream>
+// using namespace std;
+// Which nuumber is greater 
+// int main(){
+//     int Num1 ;
+//     int Num2;
+//     cout<<"Enter First Number:-" <<endl;
+//     cin>>Num1;
+//     cout<<"enter your second number:-" <<endl;
+//     cin>>Num2;
+//     if ( (Num1 > Num2)){
+//         cout<< "First NUmmber is greter than second" <<endl;
+//     }
+//     else{
+//         cout<<"second number is greater" <<endl;
+//     }
+//     return 0;
+// }   
+
+// Exapmle of this if-else Statement:-
+// here we checking age is greater than 18 or not
+//        cout<< "enter your age";
+//        cin>> age; 
+//     if(age > 18){
+//         cout<<"Can vote"<<endl;
+//     }
+//     else;{
+//         cout<<"odd number"<<endl;
+//     }
+//     return 0;
+//     }
+// one more example:-
+// int main(){
+//     int Num1 ;
+//     int Num2;
+//     cout<<"Enter First Number:-" <<endl;
+//     cin>>Num1;
+//     // cout<<"enter your second number:-" <<endl;
+//     // cin>>Num2;
+
+//     if ( (Num1/2==0)){
+//         cout<< "Even number" <<endl;
+//     }
+//     else{
+//         cout<<"Odd Number" <<endl;
+//     }
+//     return 0;
+// }
